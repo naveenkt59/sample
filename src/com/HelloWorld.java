754c7c80b6c58e16435e0583787c4f4edf700901
naveen.kmr <naveen.kmr@sonata-software.com>
2018-06-28 17:42:49 +0530

    First Commit

---
+++ src/com/HelloWorld.java
@@ -0,0 +1,20 @@
+package com;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Mahima!!!");
+		
+		add();
+
+	}
+
+	public static void add(){
+		
+		int a = 10;
+		int b = 20;
+		int c=100;
+		System.out.println(a+b-c);
+	}
+}
